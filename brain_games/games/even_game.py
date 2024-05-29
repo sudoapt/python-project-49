b0dfd057c5c04ec299d39f8828f77ce869484f3a
sudoapt <pramuk@live.ru>
2024-05-29 20:33:29 +1000

    stage5 run

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,10 @@
+import random
+
+MIN_NUM = 1
+MAX_NUM = 100
+
+
+def brain_even():
+    rand_int = random.randint(MIN_NUM, MAX_NUM)
+    answer = 'yes' if rand_int % 2 == 0 else 'no'
+    return rand_int, answer
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from brain_games.games.even_game import even_game
+
+QUESTION_ = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def main():
+    
+
+
+
+if __name__ == '__main__':
+    main()
