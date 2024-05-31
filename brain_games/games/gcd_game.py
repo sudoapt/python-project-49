63fa277e34a6d54f715cf3890fbfea80588f7767
sudoapt <pramuk@live.ru>
2024-05-31 16:27:39 +1000

    gcd build

---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,13 @@
+import random
+
+MIN_NUM = 1
+MAX_NUM = 50
+
+
+def gcd_game():
+    arg1 = random.randint(MIN_NUM, MAX_NUM)
+    arg2 = random.randint(MIN_NUM, MAX_NUM) 
+    # greatest common divisor
+    question = f"{arg1} {arg2}"
+    
+    return math, result
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from brain_games.games.gcd_game import gcd_game
+from brain_games.engine import run
+
+RULE_ = "Find the greatest common divisor of given numbers."
+
+
+def main():
+    run(gcd_game, RULE_)
+
+if __name__ == '__main__':
+    main()
