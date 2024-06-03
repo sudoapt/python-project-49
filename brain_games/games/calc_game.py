87dcef10a8d6b54c686096e731034fb2297726f4
sudoapt <pramuk@live.ru>
2024-06-03 14:51:16 +1000

    gcd done

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -8,6 +8,14 @@ def calc_game():
     arg1 = random.randint(MIN_NUM, MAX_NUM)
     arg2 = random.randint(MIN_NUM, MAX_NUM) 
     operations = random.choice(['+', '-', '*'])
+    
+    if operations == '+':
+        result = arg1 + arg2
+    elif operations == '-':
+        result = arg1 - arg2
+    elif operations == '*':
+        result = arg1 * arg2
+    
+    result_str = str(result)
     math = f"{arg1} {operations} {arg2}"
-    result = str(eval(math))
-    return math, result
+    return math, result_str
--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -1,13 +1,15 @@
 import random
+import math
 
 MIN_NUM = 1
-MAX_NUM = 50
+MAX_NUM = 100
 
 
 def gcd_game():
     arg1 = random.randint(MIN_NUM, MAX_NUM)
     arg2 = random.randint(MIN_NUM, MAX_NUM) 
-    # greatest common divisor
+    # TODO: greatest common divisor
     question = f"{arg1} {arg2}"
+    result = str(math.gcd(arg1, arg2))
     
-    return math, result
+    return question, result
