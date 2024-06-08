144d95ae0d9136bc8d816f83ac9406d2cdc4c537
sudoapt <pramuk@live.ru>
2024-06-08 21:12:06 +1000

    cli fixed

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,6 +3,6 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
-    username = prompt.string("May i have your name? ")
+    username = prompt.string("May I have your name? ")
     print(f"Hello, {username}!")
     return username
